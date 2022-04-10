60175d6945872174d5543f3672d7feaa8e2477db
LouisCaixuran <xcai67@wisc.edu>
2022-04-10 17:43:38 -0500

    add relationNode

--- node/relationNode.py
+++ node/relationNode.py
@@ -1,10 +1,24 @@
 from ../pifthon/pifthon/rwfm import objects
 from ../pifthon/pifthon import parse_json
+import storeNode
 
 """
 relation node would work like the central node to process most request and send back information with labels on it
 """
 
 class RelationNode:
-	def __init__(self,json_file,workerNodes,program):
+	def __init__(self,json_file):
 		user_inputs = parse_json.JSONParser(json_files)
+		self.globals=user_inputs.getGlobals()
+		self.storeNodes=[]
+		self.workerNodes=[]
+		self.tmpJson={
+		global_vars:[]
+		}
+		
+
+
+
+	def create_StoreNode(label,value=None):
+		
+
