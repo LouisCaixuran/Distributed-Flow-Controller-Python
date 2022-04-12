4e1bf37e8ca53b7e3db6d34013910f7229438bee
LouisCaixuran <xcai67@wisc.edu>
2022-04-12 00:22:25 -0500

    add another generate map function in python

---
+++ generate_map.py
@@ -0,0 +1,26 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+import matplotlib.cbook
+
+
+def draw_map(friends_address={"alice":(43.07167832752132, -89.40666590224633)}):
+    fig = plt.figure(num=None, figsize=(12, 8) ) 
+    m = Basemap(width=6000000,height=4500000,resolution='c',projection='aea',lat_1=35.,lat_2=45.,lon_0=-100,lat_0=40)
+    m.drawcoastlines(linewidth=0.5)
+    m.drawparallels(np.arange(-90.,91.,15.),labels=[True,True,False,False],dashes=[2,2])
+    m.drawmeridians(np.arange(-180.,181.,15.),labels=[False,False,False,True],dashes=[2,2])
+    m.drawcountries(linewidth=2, linestyle='solid', color='k' ) 
+    m.drawstates(linewidth=0.5, linestyle='solid', color='k')
+    m.shadedrelief()
+    for i in friends_address.keys():
+        x,y=m(friends_address[i][1],friends_address[i][0])
+        plt.plot(x,y,'ro', markersize=5)
+        plt.text(x,y,i,fontsize="large")
+       
+
+    plt.show()
+    return 
+
+draw_map()
