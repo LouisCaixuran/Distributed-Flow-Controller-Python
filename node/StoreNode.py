c20dd2cfbdb5b87a4346439e386286194e039284
LouisCaixuran <xcai67@wisc.edu>
2022-04-12 22:58:44 -0500

    update relation node

--- node/StoreNode.py
+++ node/StoreNode.py
@@ -1,4 +1,3 @@
-import secrets
 """
 StoreNode is a separate node to store exact information
 """
@@ -9,19 +8,12 @@ class StoreNode():
 	@owner : String  owner of the storeNode
 	@inform : exact information (in our case, it is address)
 	"""
-	def __init__(self, owner, inform ):
-  		self.owner=owner
-      self.inform=inform
+	def __init__(self, data ):
+  		self.data=data
 
-  def getOwner(key):
-    if secrets.compare_digest(key)
-    return self.owner
 
-  def getInfo():
-    return self.information
-
-  def update(newI):
-    self.inform=newI
+  def getData():
+    return self.data
 
 
 
--- node/relationNode.py
+++ node/relationNode.py
@@ -1,5 +1,7 @@
 from ../pifthon/pifthon/rwfm import objects
 from ../pifthon/pifthon import parse_json
+import json
+import secrets
 import storeNode
 
 """
@@ -7,18 +9,81 @@ relation node would work like the central node to process most request and send 
 """
 
 class RelationNode:
-	def __init__(self,json_file):
-		user_inputs = parse_json.JSONParser(json_files)
-		self.globals=user_inputs.getGlobals()
-		self.storeNodes=[]
-		self.workerNodes=[]
-		self.tmpJson={
-		global_vars:[]
-		}
+	def __init__(self,json_file,address):
+		self.user_inputs = json.load(open(json_file))
+		ids=[]
+		for i in self.user_inputs["configurations"]["global_vars"]:
+			for j in i["label"]["readers"]:
+				if j["id"] not in ids:
+					ids.append(j["id"])
+
+		self.keyMap=generateKey(ids)
+
+		with open("../keys.json", "w") as outfile:
+   	 		json.dump(self.keyMap, outfile)
 		
+		#you could find label_list and most related information in newJson
+		self.newJson=parse_json.JSONParser(json_files,data=self.user_inputs)
+
+		for i in self.newJson.globals.keys():
+			globals()[i] = createStoreNode(address[i])
 
 
 
-	def create_StoreNode(label,value=None):
-		
+		listen()
 
+
+
+
+
+
+	
+			
+
+	
+#generate random 16 digit hex to replace each label ids
+	def generateKey(self,ids):
+		keyMap={}
+		for i in ids:
+			keyMap[i]=secrets.token_hex(16)
+
+		for i in range(len(self.user_inputs["configurations"]["global_vars"])):
+			self.user_inputs["configurations"]["global_vars"][i]["owner"]=keyMap[self.user_inputs["configurations"]["global_vars"][i]["owner"]]
+			for j in range(len(self.user_inputs["configurations"]["global_vars"][i]["readers"])):
+				self.user_inputs["configurations"]["global_vars"][i]["readers"][j]["id"]=keyMap[self.user_inputs["configurations"]["global_vars"][i]["readers"][j]["id"]]
+			for j in range(len(self.user_inputs["configurations"]["global_vars"][i]["writers"])):
+				self.user_inputs["configurations"]["global_vars"][i]["readers"][j]["id"]=keyMap[self.user_inputs["configurations"]["global_vars"][i]["readers"][j]["id"]]
+
+
+		return keyMap;
+	
+
+	#Not finished, create a store node, would return the node
+	def createStoreNode(data):
+		return storeNode.StoreNode(data)
+
+
+
+	#This is in pifthon/pifthon.py, statement is python code in string
+	def run_pifthon(self,statement):
+		tokens = []
+    	temp_tokens = None
+        try:
+            temp_tokens = execute('<stdin>',statement, tokens, self.newJson)
+        except Exception:
+            return False
+        else:
+            	tokens += temp_tokens
+            # print(tokens)
+        return True
+       
+
+    #This is for communication between nodes
+    def listen():
+
+    	#TODO: get request from worker node
+
+    	if pifthon(request):
+    		#process
+    	else:
+    		#refuse
